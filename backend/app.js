f14f1cd04d8d64365d1ba4bc0289b027b281b95b
Code-With-Devansh <95998107+Code-With-Devansh@users.noreply.github.com>
2026-05-10 15:53:47 +0530

    bug

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ import { attachUser } from "./src/middleware/attachUser.js";
 import userRoute from "./src/routes/user.route.js";
 import cookieParser from "cookie-parser";
 const app = express();
-
+console.log(process.env.APP_URL)
 app.use(cors({
   origin: process.env.APP_URL,
   credentials: true,
