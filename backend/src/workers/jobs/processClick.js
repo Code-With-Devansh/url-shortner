bf6bb09196e8740d530889dee126ccd5c5a74044
Code-With-Devansh <95998107+Code-With-Devansh@users.noreply.github.com>
2026-07-02 13:20:30 +0530

    refactor: remove unused incrementClickCountToRedis import from processClick.js

--- backend/src/workers/jobs/processClick.js
+++ backend/src/workers/jobs/processClick.js
@@ -1,7 +1,6 @@
 import { UAParser } from "ua-parser-js";
 import crypto from "crypto";
 import { saveClickToRedis, minuteOf } from "../../cache/clickBucket.redis.js";
-import { incrementClickCountToRedis } from "../../cache/clicks.redis.js";
 export const processClick = async (data) => {
   const { urlId, ip, userAgent, referer, timestamp } = data;
   const ua = new UAParser(userAgent).getResult();
